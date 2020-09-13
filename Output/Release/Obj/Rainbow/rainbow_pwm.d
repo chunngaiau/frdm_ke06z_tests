322ad5242a23f9dce38c58e089d14d365995e15d
chunngaiau <chunngaiau@gmail.com>
2020-09-13 15:28:28 +0800

    Added button inputs and delay function to rainbow

--- Output/Release/Obj/Rainbow/rainbow_pwm.d
+++ Output/Release/Obj/Rainbow/rainbow_pwm.d
@@ -1,4 +1,4 @@
-Output/Debug/Obj/Rainbow/rainbow_pwm.o: \
+Output/Release/Obj/Rainbow/rainbow_pwm.o: \
  D:\User\ Files\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\KE06Z_Tests\rainbow_pwm.c \
  D:/User\ Files/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/KE06Z_Tests/Kinetis_KE/CMSIS/Device/Include/MKE06Z4.h \
  C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stdint.h \
--- rainbow_pwm.c
+++ rainbow_pwm.c
@@ -1,16 +1,20 @@
 #include <MKE06Z4.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 #define R_LED 21 // PTG5, FTM2_CH3
 #define G_LED 22 // PTG6, FTM2_CH4
 #define B_LED 23 // PTG7, FTM2_CH5
 
-#define INCREMENT 10000
+#define SW3 27 // PTH3
+#define SW2 28 // PTH4
+
+#define INCREMENT 48000
+
+void delay();
 
 int main() {
-  int counter = 0;
-
   // Enable clock gate for FTM2
   SIM_SCGC |= (1 << SIM_SCGC_FTM2_SHIFT) & SIM_SCGC_FTM2_MASK;
 
@@ -27,37 +31,69 @@ int main() {
   FTM2_C4SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
   FTM2_C5SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSA_MASK;
 
+  // Enable input buttons
+  GPIOB_PIDR ^= (1 << SW2) | (1 << SW3);
+
+  // Sets the modulo value to 100
   FTM2_MOD = 100;
   FTM2_C3V = 0;
   FTM2_C4V = 0;
   FTM2_C5V = 0;
 
   int hue;
+  bool direction;
 
   int rVal;
   int gVal;
   int bVal;
+
+  int sw2Val;
+  int sw3Val;
+
+  int lastSw3Val;
   
   for (;;) {
-    counter++;
+    delay();
 
-    if (counter == INCREMENT) {
-      counter = 0;
+    sw2Val = (GPIOB_PDIR & (1 << SW2)) >> SW2; // Press to pause animation
+    sw3Val = (GPIOB_PDIR & (1 << SW3)) >> SW3; // Press to switch animation direction
 
-      hue++;
-      if (hue >= 300) {
-        hue = 0;
-      }
-
-      rVal = (uint32_t) fmax( abs(hue - 150) -  50, 0);
-      gVal = (uint32_t) fmax(-abs(hue - 100) + 100, 0);
-      bVal = (uint32_t) fmax(-abs(hue - 200) + 100, 0);
-
-      FTM2_C3V = rVal;
-      FTM2_C4V = gVal;
-      FTM2_C5V = bVal;
+    if ((lastSw3Val == 1) && (sw3Val == 0)) { // Only register not-pressed -> pressed
+      direction = !direction;
     }
+
+    lastSw3Val = sw3Val;
+    
+    // If SW2 is pressed (==0), stop rainbow animation
+    if (sw2Val == 1) {
+      if (direction) {
+        hue++; // Goes R -> G -> B
+        if (hue >= 300) {
+          hue = 0;
+        }
+      } else {
+        hue--; // Goes R -> B -> G
+        if (hue <= 0) {
+          hue = 300;
+        }
+      }
+    }
+    
+    // Sets RGB values based on hue
+    rVal = (uint32_t) fmax( abs(hue - 150) -  50, 0);
+    gVal = (uint32_t) fmax(-abs(hue - 100) + 100, 0);
+    bVal = (uint32_t) fmax(-abs(hue - 200) + 100, 0);
+
+    FTM2_C3V = rVal;
+    FTM2_C4V = gVal;
+    FTM2_C5V = bVal;
   }
 
   return 0;
+}
+
+void delay() {
+  for (int i = 0; i < INCREMENT; i++) {
+    __ASM("NOP");
+  }
 }
